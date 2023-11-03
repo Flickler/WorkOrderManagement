f1d7a3d310e7b6ad95ae43d13bcc51a3340fdec0
Manoel Junior <107011093+Flickler@users.noreply.github.com>
2023-11-02 23:35:48 -0300

    Refactor: renamed directory

--- frontend/work-order-management/src/components/modal.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.css";
-
-export function Modal({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={styles.modal}>
-      { children }
-    </div>
-  )
-}
