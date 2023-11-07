0c3ace6d544592494f7efa659385ce6499fc7d47
Sala 307B <sala307b@ceines.com.br>
2023-11-07 19:31:43 -0300

    Creating GET detail call

--- backend/src/main/java/com/example/springboot/controllers/ChamadoController.java
+++ backend/src/main/java/com/example/springboot/controllers/ChamadoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import com.example.springboot.model.ChamadoModel;
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,59 +20,76 @@ import jakarta.validation.Valid;
 @RestController
 public class ChamadoController {
 
-	@Autowired
-	ChamadoRepository chamadoRepository;
-	
-	@PostMapping("/chamado")
-	public ResponseEntity<ChamadoModel> salvarChamado(@RequestBody @Valid ChamadoDTO chamadoDTO) {
+    @Autowired
+    ChamadoRepository chamadoRepository;
 
-		var chamadoModel = new ChamadoModel();
+    @CrossOrigin(origins = "*")
+    @PostMapping("/chamado")
+    public ResponseEntity<ChamadoModel> salvarChamado(@RequestBody @Valid ChamadoDTO chamadoDTO) {
 
-		BeanUtils.copyProperties(chamadoDTO, chamadoModel);
+        var chamadoModel = new ChamadoModel();
 
-		return ResponseEntity.status(HttpStatus.CREATED).body(chamadoRepository.save(chamadoModel));
-	}
+        BeanUtils.copyProperties(chamadoDTO, chamadoModel);
 
-	@CrossOrigin(origins = "*")
-	@GetMapping("/chamado")
-	public List<ChamadoModel> listarProdutos(){
-		return chamadoRepository.findAll();
-	}
-	/*
-	@GetMapping("/produto")
-	public ResponseEntity<Page<ProdutoModel>> listarProdutos(Pageable pageable){
-		Page<ProdutoModel> produtos = produtoRepository.findAll(pageable);
-		
-		return ResponseEntity.ok().body(produtos);
-	}*/
-	@CrossOrigin(origins = "*")
+        return ResponseEntity.status(HttpStatus.CREATED).body(chamadoRepository.save(chamadoModel));
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/chamado")
+    public List<ChamadoModel> listarChamados() {
+        return chamadoRepository.findAll();
+    }
+
+    /*
+    @GetMapping("/produto")
+    public ResponseEntity<Page<ProdutoModel>> listarProdutos(Pageable pageable){
+        Page<ProdutoModel> produtos = produtoRepository.findAll(pageable);
+
+        return ResponseEntity.ok().body(produtos);
+    }*/
+    @CrossOrigin(origins = "*")
+    @GetMapping("/chamado/{id}")
+    public ResponseEntity<ChamadoModel> listarChamadoId(@PathVariable(value = "id") Long id) {
+
+        Optional<ChamadoModel> chamado = chamadoRepository.findById(id);
+
+        if (chamado.isEmpty()) {
+            //caso nao exista ele retorna não encontrado
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(chamado.get());
+    }
+
+    @CrossOrigin(origins = "*")
     @DeleteMapping("chamado/{id}")
     public ResponseEntity<Object> deleteChamado(@PathVariable(value = "id") Long id) {
-		Optional<ChamadoModel> chamado = chamadoRepository.findById(id);
+        Optional<ChamadoModel> chamado = chamadoRepository.findById(id);
 
-		if (chamado.isEmpty()) {
-			//caso nao exista ele retorna não encontrado
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("NOT_FOUND");
-		}
-		chamadoRepository.deleteById(id);
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
-    }
-	@CrossOrigin(origins = "*")
-    @PutMapping("editar-chamado/{id}")
-    public ResponseEntity<ChamadoModel> updateChamado(@RequestBody @Valid ChamadoDTO chamadoDTO,  @PathVariable Long id) {
-		Optional<ChamadoModel> novoChamado = chamadoRepository.findById(id);
-
-		if (novoChamado.isPresent()){
-			ChamadoModel chamado = novoChamado.get();
-			chamado.setTitulo(chamadoDTO.titulo());
-			chamado.setDescricao(chamadoDTO.descricao());
-			chamado.setFuncionario(chamadoDTO.funcionario());
-			return ResponseEntity.status(HttpStatus.OK).body(chamadoRepository.save(chamado));
-		} else
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        if (chamado.isEmpty()) {
+            //caso nao exista ele retorna não encontrado
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("NOT_FOUND");
+        }
+        chamadoRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
     }
 
-	// Esse é pra mudar o status do chamado no banco
+    @CrossOrigin(origins = "*")
+    @PutMapping("chamado/{id}")
+    public ResponseEntity<ChamadoModel> updateChamado(@RequestBody @Valid ChamadoDTO chamadoDTO, @PathVariable Long id) {
+        Optional<ChamadoModel> novoChamado = chamadoRepository.findById(id);
+
+        if (novoChamado.isPresent()) {
+            ChamadoModel chamado = novoChamado.get();
+            chamado.setTitulo(chamadoDTO.titulo());
+            chamado.setDescricao(chamadoDTO.descricao());
+            chamado.setFuncionario(chamadoDTO.funcionario());
+            return ResponseEntity.status(HttpStatus.OK).body(chamadoRepository.save(chamado));
+        } else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    // Esse é pra mudar o status do chamado no banco
 	/*
 	@CrossOrigin(origins = "*")
 	@PutMapping("finalizar-chamado/{id}")
